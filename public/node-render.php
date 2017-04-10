70fd7956ec67dd05ca27ff49c25d8f7bc9cabcf8
Farolan Faisal <farolanfaisal@gmail.com>
2017-04-10 21:11:19 +0800

    add node check

--- public/node-render.php
+++ public/node-render.php
@@ -1,7 +1,11 @@
 <?php
 function renderContent($content, $moduleGroup) {
   $content = escapeshellarg($content);
-  exec("node node-render.js $content $moduleGroup", $output);
-  return join($output);
+  exec('node -v 1>/dev/null', $output, $ret);
+  if ($ret == 0) {
+    exec("node node-render.js $content $moduleGroup", $output);
+    return join($output);
+  }
+  return 'node failed';
 }
 ?>
