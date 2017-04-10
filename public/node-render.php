95bf6477703685948cfc40db43e3112b2c46c0c9
Farolan Faisal <farolanfaisal@gmail.com>
2017-04-10 15:37:17 +0800

    escape shell arg

--- public/node-render.php
+++ public/node-render.php
@@ -1,6 +1,7 @@
 <?php
 function renderContent($content, $moduleGroup) {
-  exec("node node-render.js '$content' $moduleGroup", $output);
+  $content = escapeshellarg($content);
+  exec("node node-render.js $content $moduleGroup", $output);
   return join($output);
 }
 ?>
