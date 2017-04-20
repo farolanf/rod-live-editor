9d28c16674a7ea5a9e6c10ab8e29b2bf6607ca01
Farolan Faisal <farolanfaisal@gmail.com>
2017-04-21 07:23:55 +0800

    add title

--- public/system-modules/block-include.js
+++ public/system-modules/block-include.js
@@ -2,15 +2,19 @@
   name: 'block-include',
   output: `
     <div style="background: black; color: white; text-align: center;   
-    padding: 50px;">
-      content-id %contentId%<br>
-      instance-id %instanceId%<br>
+    padding: 50px; line-height: 1.4em">
+      <small>%title%</small><br>
+      content-id <%contentId%> instance-id <%instanceId%><br>
       <a href="?id=%contentId%&instanceId=%instanceId%" 
       target="_blank">
         Edit
       </a>
     </div>`,
   properties: {
+    title: {
+      type: 'text',
+      default: 'External Content',
+    },
     contentId: {
       type: 'property',
       default: '',
