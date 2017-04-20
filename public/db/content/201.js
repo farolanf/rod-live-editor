6c6fdd92a351fe2ec795cd1695da0c4a9aaf78d4
Farolan Faisal <farolanfaisal@gmail.com>
2017-04-20 15:32:39 +0800

    improve sample

--- public/db/content/201.js
+++ public/db/content/201.js
@@ -50,6 +50,11 @@
             "hiddenPreheader": "test",
             "body": [
                 {
+                    name: 'block-include',
+                    contentId: 202,
+                    instanceId: 2 
+                },
+                {
                 "name": "1-column",
                 "textAlign": "left",
                 "padding": "30px 40px 0",
@@ -81,11 +86,6 @@
                 "content": [{
                     "name": "block-text",
                     "text": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing "
-                },
-                {
-                    name: 'block-include',
-                    contentId: 202,
-                    instanceId: 2 
                 },
                 {
                     "name": "block-text",
