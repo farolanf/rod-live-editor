2b0db40c89229226afd25606c99d774e7a1f6cdc
Farolan Faisal <farolanfaisal@gmail.com>
2017-04-20 18:02:40 +0800

    add save before precompile

--- public/utils/ui.js
+++ public/utils/ui.js
@@ -28,7 +28,8 @@ function UIUtils() {
    * @param {string} onclick - The code to be executed on click.
    * @param {string} type - Type of message <undefined|danger>.
    */
-  this.showConfirmModal = function(title, msg, actionTitle, onclick, type) {
+  this.showConfirmModal = function(title, msg, actionTitle, onclick, type, 
+  actionTitle2, onclick2, type2) {
     const modal = $('#confirm-modal');
     $('.modal-title', modal).text(title);
     $('.confirm-msg', modal).text(msg);
@@ -38,6 +39,14 @@ function UIUtils() {
     }
     else {
       $('[data-primary-btn]', modal).removeClass('btn-danger').addClass('btn-primary');
+    }
+    const btn3 = $('[data-third-btn]', modal);
+    btn3.toggleClass('hidden', !actionTitle2);
+    if (!!actionTitle2) {
+      btn3.text(actionTitle2).attr('onclick', onclick2)
+        .toggleClass('btn-primary', type2 === 'primary')
+        .toggleClass('btn-danger', type2 === 'danger')
+        .toggleClass('btn-success', type2 === 'success');
     }
     modal.modal();
   };
