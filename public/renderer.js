3cdf9be78932b984a20ec1ffb036174e653b1855
Farolan Faisal <farolanfaisal@gmail.com>
2017-05-05 09:37:23 +0800

    remove unused code

--- public/renderer.js
+++ public/renderer.js
@@ -24,100 +24,17 @@ function Renderer(modules, globalProperties, language) {
     removeJsComments,
   };
 
-  function removeJsComments(str) {
-    return str.replace(/\/\*[^*]*?\*\//g, '')
-      .replace(/(.+\s*)\/\/[^\n]*?(\n|$)/g, '$1$2') // keep new line
-      .replace(/$\s*\/\/[^\n]*?(\n|$)/g, ''); // eat new line
-  }
-
   /**
    * Remove javascript comments from a string.
    * 
    * @param {string} str - The string to search for comments.
+   * @return {string} - The str with removed comments.
    * @private
    */
-  function _removeJsComments(str) {
-    str = ('__' + str + '__').split('');
-    var mode = {
-      singleQuote: false,
-      doubleQuote: false,
-      regex: false,
-      blockComment: false,
-      lineComment: false,
-      condComp: false
-    };
-    for (var i = 0, l = str.length; i < l; i++) {
-
-      if (mode.regex) {
-        if (str[i] === '/' && str[i - 1] !== "\\") {
-          mode.regex = false;
-        }
-        continue;
-      }
-
-      if (mode.singleQuote) {
-        if (str[i] === "'" && str[i - 1] !== "\\") {
-          mode.singleQuote = false;
-        }
-        continue;
-      }
-
-      if (mode.doubleQuote) {
-        if (str[i] === '"' && str[i - 1] !== "\\") {
-          mode.doubleQuote = false;
-        }
-        continue;
-      }
-
-      if (mode.blockComment) {
-        if (str[i] === '*' && str[i + 1] === '/') {
-          str[i + 1] = '';
-          mode.blockComment = false;
-        }
-        str[i] = '';
-        continue;
-      }
-
-      if (mode.lineComment) {
-        if (str[i + 1] === 'n' || str[i + 1] === 'r') {
-          mode.lineComment = false;
-        }
-        str[i] = '';
-        continue;
-      }
-
-      if (mode.condComp) {
-        if (str[i - 2] === '@' && str[i - 1] === '*' && str[i] === '/') {
-          mode.condComp = false;
-        }
-        continue;
-      }
-
-      mode.doubleQuote = str[i] === '"';
-      mode.singleQuote = str[i] === "'";
-
-      if (str[i] === '/') {
-
-        if (str[i + 1] === '*' && str[i + 2] === '@') {
-          mode.condComp = true;
-          continue;
-        }
-        if (str[i + 1] === '*') {
-          str[i] = '';
-          mode.blockComment = true;
-          continue;
-        }
-        if (str[i + 1] === '/') {
-          str[i] = '';
-          mode.lineComment = true;
-          continue;
-        }
-        mode.regex = true;
-
-      }
-
-    }
-    return str.join('').slice(2, -2);
+  function removeJsComments(str) {
+    return str.replace(/\/\*[^*]*?\*\//g, '')
+      .replace(/(.+\s*)\/\/[^\n]*?(\n|$)/g, '$1$2') // keep new line
+      .replace(/$\s*\/\/[^\n]*?(\n|$)/g, ''); // eat new line
   }
 
   /**
